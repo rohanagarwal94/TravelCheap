76e75edd1ea46393c76a29799d256e0006289642
imRishabhGupta <rishabhgpt86@gmail.com>
2016-10-01 19:54:12 +0530

    Added directions api.

--- app/src/main/java/com/codeslayers/hack/travelcheap/MainActivity.java
+++ app/src/main/java/com/codeslayers/hack/travelcheap/MainActivity.java
@@ -8,18 +8,34 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
     
     int PLACE_AUTOCOMPLETE_REQUEST_CODE1 = 1;
     int PLACE_AUTOCOMPLETE_REQUEST_CODE2 = 2;
 
+    private RequestQueue requestQueue;
+    private String url;
 
     TextView e1,e2;
 
@@ -63,7 +79,72 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        url="https://maps.googleapis.com/maps/api/directions/json?origin=shahdara&destination=preet%20vihar&mode=transit&transit_mode=bus&key=AIzaSyDuZ2e5qarM-fhwOoAS4WNum1k1Ow2lhLs";
+        getJson();
     }
+
+    public void getJson(){
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+        JsonObjectRequest jor = new JsonObjectRequest(url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println("Its working");
+                        try{
+                            System.out.println(response.toString());
+                            JSONArray routes = response.getJSONArray("routes");
+                            System.out.println(routes.length());
+                            JSONObject route=routes.getJSONObject(0);
+                            JSONArray legs = route.getJSONArray("legs");
+                            System.out.println("legs "+legs.length());
+                            JSONObject jsonObject = legs.getJSONObject(0);
+                            JSONArray steps = jsonObject.getJSONArray("steps");
+                            for(int j=0;j<steps.length();j++){
+                                JSONObject jsonObject1 = steps.getJSONObject(j);
+                                String travelMode=jsonObject1.getString("travel_mode");
+                                System.out.println(travelMode);
+                                if(travelMode.equals("WALKING"))
+                                    continue;
+
+                                String duration=jsonObject1.getString("duration");
+                                String htmlInstructions=jsonObject1.getString("html_instructions");
+                                JSONObject transitDetails=jsonObject1.getJSONObject("transit_details");
+                                JSONObject arrivalStop=transitDetails.getJSONObject("arrival_stop");
+                                JSONObject departureStop=transitDetails.getJSONObject("departure_stop");
+                                JSONObject line=transitDetails.getJSONObject("line");
+                                JSONObject vehicle=line.getJSONObject("vehicle");
+                                String type=vehicle.getString("type");
+                                String arrivalName=arrivalStop.getString("name");
+                                String departureName=departureStop.getString("name");
+                                System.out.println(departureName+" to "+arrivalName+" via "+type);
+                            }
+                            System.out.println(steps.length());
+                        }catch(JSONException e){e.printStackTrace();}
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //progressDialog.dismiss();
+                        Toast.makeText(getApplicationContext(),"Error in loading all posts.",Toast.LENGTH_LONG).show();
+                        Log.e("Volley",error.toString());
+
+                    }
+                }
+        ){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                System.out.println("here it is");
+                return headers;
+            }
+
+        };
+        requestQueue.add(jor);
+
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
